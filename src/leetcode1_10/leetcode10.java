a9f14b4317d42aa4ceb5e0e9d3112e09b097453b
Lin <weihaolin2008@qq.com>
2018-01-12 22:53:55 +0800

    Leetcode10添加解释注释

--- src/leetcode1_10/leetcode10.java
+++ src/leetcode1_10/leetcode10.java
@@ -1,5 +1,10 @@
 package leetcode1_10;
-
+/**
+ * 容器的体积只取决于底(两点于x轴的距离)和高(较低木板的高,eg.短板理论)
+ * 		故每次计算area后能从短板一侧去除更短的木板以实现剪枝的作用{while (i < j && height[i] <= h) i++;}
+ * @author god
+ *
+ */
 public class leetcode10 {
 
 	public int maxArea(int[] height) {
